25ce781aade91c3db1f4f1e5b7412c937f3d541d
Sebastian R. Papanikolaou Costa <sebastian.papanikolaou@protonmail.com>
2024-07-22 18:45:00 -0400

    Delete dev1.sh

--- sql-server/dev1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# make back up
-
-query_result=$(/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Your_Strong@Password1 -d APP_DATA -Q "select * from [RAW_DATA].[INTENSITY_APP_FILE];" )
-
-cat "$query_result"
